619ba2a3fffdffeb7b7b633fd7fb59eb789a9b76
jyadav <jayesh.yadav@netcracker.com>
2020-05-08 17:07:44 +0530

    Modified home controller

--- src/main/java/com/jayesh/demo/openshift/container/HomeController.java
+++ src/main/java/com/jayesh/demo/openshift/container/HomeController.java
@@ -1,6 +1,7 @@
 package com.jayesh.demo.openshift.container;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HomeController {
 	@GetMapping("/")
 	public String welcomeMsg() {
 		return "Welcome Sneha Darling...!!!";
+	}
+	
+	@GetMapping("/{input}")
+	public String withPathVariable(@PathVariable String input) {
+		return "The path variable passed is :"+input;
 	}
 }
